944a640a132c8b84aa8983d824deebc3f25d3dc0
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-24 11:50:23 -0400

    start module

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
-
-
 (function() {
-    let game = {
-        gameBoard: [{name: a, value: 0}, {name: b, value: 0}, {name: c, value: 0}, {name: d, value: 0}, {name: e, value: 0}, {name: f, value: 0}, {name: g, value: 0}, {name: h, value: 0}, {name: i, value: 0}],
+    var     game = {
+        // Make baord 
+        gameBoard: [{name: "a", value: 0}, {name: "b", value: 0}, {name: "c", value: 0}, {name: "d", value: 0}, {name: "e", value: 0}, {name: "f", value: 0}, {name: "g", value: 0}, {name: "h", value: 0}, {name: "i", value: 0}],
+
+        // Init 
+        init: function(){
+        },
+        
     }
+    game.init();
 })();
+
