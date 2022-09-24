14fdd9ee3dd55b0197b5be572b07153072202b16
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-24 11:31:35 -0400

    Make board

---
+++ app.js
@@ -0,0 +1,7 @@
+
+
+(function() {
+    let game = {
+        gameBoard: [{name: a, value: 0}, {name: b, value: 0}, {name: c, value: 0}, {name: d, value: 0}, {name: e, value: 0}, {name: f, value: 0}, {name: g, value: 0}, {name: h, value: 0}, {name: i, value: 0}],
+    }
+})();
