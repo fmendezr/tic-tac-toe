fd189132d72f87b802c2bfbf408a0a8dfa685cac
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-24 13:56:06 -0400

    Add cacheDom, binding function and start play

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
 (function() {
     var     game = {
         // Make baord 
-        gameBoard: [0, 0, 0, 1, 0, 0, 0, 0, 1],
+        gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
         turns: 0,
 
-        // Init 
+        // initializes the module
         init: function(){
-            this.render()
+            this.cacheDom();
+            this.bindEvents();
+            this.render();
         },
 
-        // Render
+        // Only thing that caches DOM 
+        cacheDom:  function(){
+            this.tiles = document.querySelectorAll(".tile")
+        },
+
+        // Only thing that touches the DOM itself
+        bindEvents: function(){
+            this.tiles.forEach(tile => {
+                tile.addEventListener("click", this.play.bind(this));
+            });
+        },
+
+        // Only part of 
         render: function(){
-            const tiles = document.querySelectorAll(".tile")
             for (let i = 0; i < 9; i++){
                 if (this.gameBoard[i] != 0){
-                    tiles[i].textContent = "X";
+                    this.tiles[i].textContent = "X";
                 }
             }
         },
 
         // determine whether it's X or O turn  
-        WhosTurn: function(){
+        whosTurn: function(){
             if (this.turns % 2 == 1){
                 return 1;
             } else if (this.turns % 2 == 0){
@@ -28,6 +41,10 @@
             }
         },
 
+        // add button listners to play
+        play: function(){
+            console.log(this.gameBoard)
+        }
     };
     game.init();
 })();
