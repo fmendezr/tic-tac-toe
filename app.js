718f252156e249a4970cd44a53625da08b303089
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-25 14:01:32 -0400

    Restructure app.js

--- app.js
+++ app.js
@@ -1,59 +1,59 @@
-(function() {
-    var     game = {
-        // Make baord 
-        gameBoard: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-        turns: 0,
-        
-        // initializes the modulez
-        init: function(){
-            this.cacheDom();
-            this.bindEvents();
-            this.render();
-        },
+// only thing that caches stuff from the dom 
+const cacheDom = (function() {
+    const tiles = document.querySelectorAll(".tile")
+    return {tiles}
+})()
 
-        // Only thing that caches DOM 
-        cacheDom:  function(){
-            this.tiles = document.querySelectorAll(".tile")
-        },
 
-        // Binds events to dom elements 
-        bindEvents: function(){
-            this.tiles.forEach(tile => {
-                tile.addEventListener("click", this.play);
+// Player factory
+const playerFactory = (name, number, symbol) => {
+    return {name, number, symbol};
+};
 
-            });
-        },
+// Create players 
+const Patrick = playerFactory("Patrick", 1, "X");
+const Daemon = playerFactory("Daemon", 2, "O");
 
-        // Only thing that alters'0 the DOM itself
-        render: function(){
-            for (let i = 0; i < 9; i++){
-                if (this.gameBoard[i] == 1){
-                    this.tiles[i].textContent = "X"
-                } else if (this.gameBoard[i] == -1){
-                    this.tiles[i].textContent = "O";
-                }  
-            }
-        },
-
-        // determine whether it's X or O turn  
-        whosTurn: function(){
-            if (this.turns % 2 == 1){
-                return 1;
-            } else if (this.turns % 2 == 0){
-                return -1;
-            }
-        },
-
-        // add button listners to play
-        whichButtonWasClicked: function(){
-            globalThis.play(this.id)
-        },
-
-        play: function(){
-            console.log(this.id)
-        }
-
-    };
-    game.init();
+// Game board module object 
+let  gameBoardModule =  (function() {
+    let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    return {gameBoard}
 })();
 
+// only thing that alters the dom
+let renderModule = (function() {
+    render = function(){
+        for(let i = 0; i < 9; i++){
+            if(gameBoardModule.gameBoard[i] != 0){
+                cacheDom.tiles[i].textContent = gameBoardModule.gameBoard[i];
+            } 
+        }
+    }
+    return{render};
+})();
+
+// Game module that handles flow of the game
+let game = (function() {
+    turns = 0;
+
+    // determine wwhether an X or an O is to be placed 
+    let determineSymbol = function(){
+        if(turns % 2 == 0){
+            symbol = Patrick.symbol;
+        } else {
+            symbol = Daemon.symbol;
+        }
+        turns += 1; 
+        return symbol;
+    };
+
+    // places x or y on empty tiles  
+    cacheDom.tiles.forEach(element => {
+        element.addEventListener("click", () => {
+            if(gameBoardModule.gameBoard[element.id] == 0){
+                gameBoardModule.gameBoard[element.id] = determineSymbol();
+                renderModule.render()
+            }
+        })
+    }); 
+})()
