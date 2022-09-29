cb19d6c860cf27b9c2130277988917d4181dfa40
Felix Mendez <mendez.felixrn@gmail.com>
2022-09-29 14:41:33 -0400

    Reestructure code so it is more organized

--- app.js
+++ app.js
@@ -1,117 +1,171 @@
-// only thing that caches stuff from the dom 
-const cacheDom = (function() {
+// gameboard object 
+const GameBoard = {
+    gameBoard: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+    getEmptyIndexes: function(){
+        return (this.gameBoard.filter(element => typeof(element) ==  "number"))
+    }
+}
+
+// only things that fetches things from the dom 
+const cacheDom = (function(){
     const tiles = document.querySelectorAll(".tile");
-    const resetBtn = document.getElementById("resetBtn");
-    const resultModal = document.getElementById("result-modal");
-    const textResultModal = document.getElementById("result-printer");
-    const playerNames = document.querySelectorAll(".name");
     const initialModal = document.getElementById("initial-modal");
-    const startBtn = document.getElementById("startBtn");
-    return {tiles, resetBtn, resultModal, textResultModal, playerNames, initialModal, startBtn}
-})()
-
-
-// Player factory
-const playerFactory = (name, number, symbol) => {
-    return {name, number, symbol, changeName(newName){
-        this.name = newName;
-        return this;
-    }};
-};
-
-// Create Players 
-const Patrick = playerFactory('Patrick', 1, "X");
-const Driver = playerFactory("Driver", 2, "O");
-
-// Game board module object 
-let  gameBoardModule =  (function() {
-    let gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-    return {gameBoard}
+    const resutlModal = document.getElementById("result-modal");
+    const resultPrinter = document.getElementById("result-printer");
+    const startBtn = document.getElementById("start-btn");
+    const resetBtn = document.getElementById("reset-btn")
+    const aiCheckbox = document.getElementById("ai-checkbox");
+    const namePlayerOne = document.getElementById("name-player-one");
+    const namePlayerTwo = document.getElementById("name-player-two");
+    return {tiles, initialModal, resutlModal, resultPrinter, startBtn, resetBtn, aiCheckbox, namePlayerOne, namePlayerTwo}
 })();
 
-// only thing that alters the dom
-let renderModule = (function() {
-    render = function(){
+// only thing that directly interacts with dom (renders and unrenders stuff)
+const renderModule = (function(){
+    const renderGameBoard = () => {
         for(let i = 0; i < 9; i++){
-            if(gameBoardModule.gameBoard[i] != 0){
-                cacheDom.tiles[i].textContent = gameBoardModule.gameBoard[i];
-            } 
+            if(typeof(GameBoard.gameBoard[i]) !=  "number"){
+                cacheDom.tiles[i].textContent = GameBoard.gameBoard[i];
+            }
         }
     }
-    return{render};
+    const unrenderGameBoard = () => {
+        for(let i=0; i < 9; i++){
+            cacheDom.tiles[i].textContent = "";
+        }
+    }
+    const renderElement = (modal) => {
+        modal.classList.add("active");
+    }
+    const unrenderElement = (target) => {
+        target.classList.remove("active");
+    }
+
+    const changeTextContent = (target, newContent) => {
+        target.textContent = newContent;
+    } 
+
+    return {renderGameBoard, unrenderGameBoard, renderElement, unrenderElement, changeTextContent}
 })();
 
-// Game module that handles flow of the game
-let game = (function() {
-    turns = 0;
+// handles everything related to player creation
+const createPlayersModule = (function(){
 
-    //create players 
-    cacheDom.startBtn.addEventListener("click", () => {
-        Patrick.changeName(cacheDom.playerNames[0].value);
-        Driver.changeName(cacheDom.playerNames[1].value);
-        cacheDom.initialModal.classList.remove("active");
-        cacheDom.resetBtn.classList.add("active");
-    })
-
-    // restart everything
-    cacheDom.resetBtn.addEventListener("click", () => {
-        gameBoardModule.gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-        cacheDom.tiles.forEach(tile => tile.textContent = "");
-        renderModule.render();
-        turns = 0;
-        cacheDom.playerNames[0].value = "";
-        cacheDom.playerNames[1].value = "";
-        cacheDom.initialModal.classList.add("active");
-        cacheDom.resetBtn.classList.remove("active");
-        cacheDom.resultModal.classList.remove("active");
-    })
-
-    // determine wwhether an X or an O is to be placed 
-    const determineTurn = function(){
-        if(turns % 2 == 0){
-            player = Patrick;
-        } else {
-            player = Driver;
+    const userFactory = (name, symbol, type="human") => {
+        return {name, symbol, type};
+    }
+    
+    const aiFactory = (name, symbol, type="ai") => {
+        const makeRandomMove = function(){
+            return (Math.floor(Math.random() * 10)); 
         }
-        turns += 1; 
-        return player;
-    };
+        return {name, symbol, type, makeRandomMove}
+    }
 
-    // determines if someone has won 
-    const winner = function(){
-        combinations = [
-                      [cacheDom.tiles[0].textContent, cacheDom.tiles[1].textContent, cacheDom.tiles[2].textContent], 
-                      [cacheDom.tiles[3].textContent, cacheDom.tiles[4].textContent, cacheDom.tiles[5].textContent], 
-                      [cacheDom.tiles[6].textContent, cacheDom.tiles[7].textContent, cacheDom.tiles[8].textContent], 
-                      [cacheDom.tiles[0].textContent, cacheDom.tiles[3].textContent, cacheDom.tiles[6].textContent], 
-                      [cacheDom.tiles[1].textContent, cacheDom.tiles[4].textContent, cacheDom.tiles[7].textContent], 
-                      [cacheDom.tiles[2].textContent, cacheDom.tiles[5].textContent, cacheDom.tiles[8].textContent], 
-                      [cacheDom.tiles[0].textContent, cacheDom.tiles[4].textContent, cacheDom.tiles[8].textContent],
-                      [cacheDom.tiles[6].textContent, cacheDom.tiles[4].textContent, cacheDom.tiles[2].textContent]
-                      ];
-      arr = [];
-      for(let i = 0; i < combinations.length; i++){
-          arr.push(combinations[i].every(element => element == combinations[i][0] && combinations[i][0] != 0));
-      }
-      return arr.some(element => element == true)
-    };
-
-    // places x or y on empty tiles  
-    cacheDom.tiles.forEach(element => {
-        element.addEventListener("click", () => {
-            if(gameBoardModule.gameBoard[element.id] == 0){
-                gameBoardModule.gameBoard[element.id] = determineTurn().symbol;
-                renderModule.render()
-                if(winner() == true){
-                    turns -= 1;
-                    cacheDom.textResultModal.textContent = `${determineTurn().name} Won`;
-                    cacheDom.resultModal.classList.add("active");
-                }
-                if(turns == 9){
-                    cacheDom.textResultModal.textContent = "Draw";
-                    cacheDom.resultModal.classList.add("active");
-                }
-            }
-        })
-    });
+    const createPlayers = () => {
+        GameObject.players.push(userFactory(cacheDom.namePlayerOne.value, "X"));
+        if(cacheDom.aiCheckbox.checked == true){
+            GameObject.players.push(aiFactory(cacheDom.namePlayerTwo.value, "O"));
+        } else {
+            GameObject.players.push(userFactory(cacheDom.namePlayerTwo.value, "O"));
+        }
+    }
+    return {createPlayers}
 })()
+
+// clears everything 
+const clearModule = (function(){
+    const clearEverything = () => {
+        GameBoard.gameBoard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+        cacheDom.namePlayerOne.value = "";
+        cacheDom.namePlayerTwo.value = "";
+        cacheDom.aiCheckbox.checked = false;
+        GameObject.players = [];
+        GameObject.turns = 0;
+    }
+    return {clearEverything}
+})()
+
+// binds events to dom elements  
+const binderModule = (function(){
+    cacheDom.tiles.forEach(tile => tile.addEventListener("click", () => {GameObject.userTurn(tile.id)}));
+    cacheDom.startBtn.addEventListener("click", () => {
+        createPlayersModule.createPlayers();
+        renderModule.unrenderElement(cacheDom.initialModal);
+        renderModule.renderGameBoard();
+        renderModule.renderElement(cacheDom.resetBtn);
+    });
+    cacheDom.resetBtn.addEventListener("click", () => {
+        clearModule.clearEverything();
+        renderModule.unrenderElement(cacheDom.resetBtn);
+        renderModule.unrenderElement(cacheDom.resutlModal);
+        renderModule.renderElement(cacheDom.initialModal);
+        renderModule.unrenderGameBoard()
+    })
+})();
+
+
+// object that handles the game 
+const GameObject = {
+    players: [],
+    turns: 0,
+
+    checkWinner: function(player){
+        if (
+            GameBoard.gameBoard[0] == player.symbol && GameBoard.gameBoard[1] == player.symbol && GameBoard.gameBoard[2] == player.symbol ||
+            GameBoard.gameBoard[3] == player.symbol && GameBoard.gameBoard[4] == player.symbol && GameBoard.gameBoard[5] == player.symbol ||
+            GameBoard.gameBoard[6] == player.symbol && GameBoard.gameBoard[7] == player.symbol && GameBoard.gameBoard[8] == player.symbol ||
+            GameBoard.gameBoard[0] == player.symbol && GameBoard.gameBoard[3] == player.symbol && GameBoard.gameBoard[6] == player.symbol ||
+            GameBoard.gameBoard[1] == player.symbol && GameBoard.gameBoard[4] == player.symbol && GameBoard.gameBoard[7] == player.symbol ||
+            GameBoard.gameBoard[2] == player.symbol && GameBoard.gameBoard[5] == player.symbol && GameBoard.gameBoard[8] == player.symbol ||
+            GameBoard.gameBoard[0] == player.symbol && GameBoard.gameBoard[4] == player.symbol && GameBoard.gameBoard[8] == player.symbol ||
+            GameBoard.gameBoard[2] == player.symbol && GameBoard.gameBoard[4] == player.symbol && GameBoard.gameBoard[6] == player.symbol 
+        ){
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    activePlayer: function(){   
+        if(this.turns % 2 == 0){
+            return this.players[0];
+        } else {
+            return this.players[1];
+        }
+    },
+
+    userTurn: function(buttonClicked){
+        if(GameBoard.getEmptyIndexes().includes(parseInt(buttonClicked))){
+            GameBoard.gameBoard[buttonClicked] = this.activePlayer().symbol;
+            renderModule.renderGameBoard();
+            this.gameflow();
+        }
+    },
+
+    aiTurn: function(){
+        index = this.players[1].makeRandomMove()
+        if (GameBoard.getEmptyIndexes().includes(index)){
+            GameBoard.gameBoard[index] = this.players[1].symbol;
+            renderModule.renderGameBoard();
+            this.gameflow();
+        } else{
+            this.aiTurn();
+        }   
+    },
+
+    gameflow: function(){
+        if(this.checkWinner(this.activePlayer())){
+            renderModule.changeTextContent(cacheDom.resultPrinter, `${this.activePlayer().name} Won`);
+            renderModule.renderElement(cacheDom.resutlModal)   
+        } 
+        this.turns += 1  
+        if(GameBoard.getEmptyIndexes().length == 0){
+            renderModule.changeTextContent(cacheDom.resultPrinter, "Draw");
+            renderModule.renderElement(cacheDom.resutlModal)  
+        }
+        if (this.players[1].type == "ai" && this.activePlayer() == this.players[1] && GameBoard.getEmptyIndexes().length > 0){
+            this.aiTurn();
+        }
+    }
+}
